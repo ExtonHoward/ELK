df54383084140cb7b7546a036c034bf0a753bcc2
Exton howard <exton.howard@gmail.com>
2021-05-19 21:47:52 -0400

    misc bash scripts

---
+++ Linux/bash-scripts/Misc-scripts/Setup_evidence_Files.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#This script will set up the files you'll need to examine for evidence
+
+#Make sure to chmod this file
+#chmod +x ./Setup_Evidence_Files.sh
+
+#Then run the script
+#./Setup_Evidence_Files.sh
+
+#Create evidence folders and files into the current directory
+mkdir -p Dealer_Schedules_0310
+mkdir -p Roulette_Player_WinLoss_0310
